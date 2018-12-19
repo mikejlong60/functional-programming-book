10bceb761dbf4637c1b0c61674ff2bd78ca552b8
michael.long <michael.long@deciphernow.com>
2018-12-19 09:58:14 -0500

    Magit test

--- src/test/scala/chapter3/Tree.scala
+++ src/test/scala/chapter3/Tree.scala
@@ -5,7 +5,7 @@ sealed trait Tree[+A]
 case class Leaf[A](value: A) extends Tree[A]
 
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
-
+//
 object Tree {
   def size[A](tree: Tree[A]): Int = tree match {
     case Branch(l: Branch[A], r: Branch[A])  => 2 + size(l) + size(r)

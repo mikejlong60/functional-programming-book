54cd2ec1b1a1c9170f8c478f26ee1f4b44125101
michael.long <michael.long@deciphernow.com>
2019-01-08 10:55:13 -0500

    Adds comment from answers to exercices on internet

--- src/test/scala/chapter3/Tree.scala
+++ src/test/scala/chapter3/Tree.scala
@@ -11,33 +11,36 @@ case object NilNode extends Tree[Nothing]
 object Tree {
   import scala.math.max
 
-
   //Old implementations, not in terms of Fold
-//  def map[A,B](t: Tree[A])(f: A => B): Tree[B] = t match {
-//    case Leaf(a) => Leaf(f(a))
-//    case Branch(l,r) => Branch(map(l)(f), map(r)(f))
-//  }
-//
-//  def maximum(t: Tree[Int]): Int = t match {
-//    case Leaf(n) => n
-//    case Branch(l,r) => maximum(l) max maximum(r)
-//  }
-//
-//  def depth[A](t: Tree[A]): Int = t match {
-//    case Leaf(_) => 0
-//    case Branch(l,r) => 1 + (depth(l) max depth(r))
-//  }
-//
-//  def size[A](tree: Tree[A]): Int = tree match {
-//    case Leaf(_) => 1
-//    case Branch(l, r) => 1 + size(l) + size(r)
-//  }
+  //  def map[A,B](t: Tree[A])(f: A => B): Tree[B] = t match {
+  //    case Leaf(a) => Leaf(f(a))
+  //    case Branch(l,r) => Branch(map(l)(f), map(r)(f))
+  //  }
+  //
+  //  def maximum(t: Tree[Int]): Int = t match {
+  //    case Leaf(n) => n
+  //    case Branch(l,r) => maximum(l) max maximum(r)
+  //  }
+  //
+  //  def depth[A](t: Tree[A]): Int = t match {
+  //    case Leaf(_) => 0
+  //    case Branch(l,r) => 1 + (depth(l) max depth(r))
+  //  }
+  //
+  //  def size[A](tree: Tree[A]): Int = tree match {
+  //    case Leaf(_) => 1
+  //    case Branch(l, r) => 1 + size(l) + size(r)
+  //  }
 
   //Note the trick here. I can generally replace pattern matching with one function per pattern.
-
-  def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B)(h: () => B): B = t match {
+  //From answers:
+  /* Like `foldRight` for lists, `fold` receives a "handler" for each of the data constructors of the type, and recursively
+    accumulates some value using these handlers. As with `foldRight`, `fold(t)(Leaf(_))(Branch(_,_)) == t`, and we can use
+    this function to implement just about any recursive function that would otherwise be defined by pattern matching.
+ */
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B)(h: () => B): B = t match {
     case Leaf(a) => f(a)
-    case Branch(l,r) => g(fold(l)(f)(g)(h), fold(r)(f)(g)(h))
+    case Branch(l, r) => g(fold(l)(f)(g)(h), fold(r)(f)(g)(h))
     case NilNode => h()
   }
 
@@ -45,7 +48,7 @@ object Tree {
 
   def maximum(t: Tree[Int]): Int = fold(t)(a => a)((b1, b2) => b1 max b2)(() => Int.MinValue)
 
-  def depth[A](t: Tree[A]): Int = fold(t)(a => 0)((d1,d2) => 1 + (d1 max d2))(() => 0)
+  def depth[A](t: Tree[A]): Int = fold(t)(a => 0)((d1, d2) => 1 + (d1 max d2))(() => 0)
 
-  def map[A,B](t: Tree[A])(f: A => B): Tree[B] = fold(t)(a => Leaf(f(a)): Tree[B])(Branch(_,_))(() => NilNode: Tree[B])
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = fold(t)(a => Leaf(f(a)): Tree[B])(Branch(_, _))(() => NilNode: Tree[B])
 }

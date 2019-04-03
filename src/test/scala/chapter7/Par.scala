431f228bb788cf5e808b06bff98bcbb486ffe015
Michael Long <michael.long@deciphernow.com>
2019-04-03 16:51:04 -0400

    Adds a couple more exercises

--- src/test/scala/chapter7/Par.scala
+++ src/test/scala/chapter7/Par.scala
@@ -7,7 +7,9 @@ object Par {
 
   type Par[A] = ExecutorService => Future[A]
 
-  def unit[A](a:  => A): Par[A] = (es: ExecutorService) => UnitFuture(a)
+  def unit[A](a:  A): Par[A] = (es: ExecutorService) => UnitFuture(a)
+
+  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
 
   private case class UnitFuture[A](get: A) extends Future[A] {
     def isDone = true
@@ -16,11 +18,14 @@ object Par {
     def cancel(evenIfRunning: Boolean): Boolean = false
   }
 
-  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
+  def map2[A, B, C](a: Par[A], b: Par[B], timeoutMillis: Long)(f: (A, B) => C): Par[C] =
     (es: ExecutorService) => {
       val af = a(es)
       val bf = b(es)
-      UnitFuture(f(af.get, bf.get))
+      val start = System.currentTimeMillis
+      val aff = af.get(timeoutMillis, TimeUnit.MILLISECONDS)
+      val bff = bf.get(timeoutMillis - (System.currentTimeMillis - start), TimeUnit.MILLISECONDS)
+      UnitFuture(f(aff, bff))
     }
 
   def fork[A](a: => Par[A]): Par[A] =
@@ -28,6 +33,7 @@ object Par {
           def call = a(es).get
         })
     
+  def asyncF[A, B](f: A => B): A => Par[B] = ???
 
   def sumInParallelNot(ints: List[Int]): Int = {
     if (ints.size <= 1) ints.headOption getOrElse 0
@@ -37,20 +43,13 @@ object Par {
     }
   }
 
-//  def sumInParallel[A](ints: List[Int], myPar: Par[Int]): Int = {
-//    if (ints.size <= 1) {
-//      val p = myPar.unit(ints.headOption getOrElse 0)
-//      12//myPar.get(p)// This is dumb
-//    } else {
-//      val (l, r) = ints.splitAt(ints.length/2)
-//      sumInParallel(l, myPar) + sumInParallel(r, myPar)
-//    }
-//  }
-
-
+  def sumInParallel[A](ints: List[Int]): Par[Int] = {
+    if (ints.size <= 1) {
+      val p = Par.unit(ints.headOption getOrElse 0)
+      p
+    } else {
+      val (l, r) = ints.splitAt(ints.length/2)
+      Par.map2(Par.fork(sumInParallel(l)), Par.fork(sumInParallel(r)), 1000)((x, y) => x + y)
+    }
+  }
 }
-
-//case class MyPar[String](x: String) extends Par[String] {
-////  def unit(a: => String): MyPar[String] = MyPar(a)
-//  def get(a: Par[String]):String =  ???
-//}
--- src/test/scala/chapter7/ParTest.scala
+++ src/test/scala/chapter7/ParTest.scala
@@ -4,15 +4,34 @@ import org.scalatest.prop.PropertyChecks
 import org.scalatest.{Matchers, PropSpec}
 import org.scalactic.TypeCheckedTripleEquals._ 
 import Par._
+import java.util.concurrent._
+
 
 class ParTest extends PropSpec with PropertyChecks with Matchers {
+  val executor = Executors.newFixedThreadPool(10)
 
-  property("Sum in parallel not ") {
+  property("Sum in parallel NOT ") {
     forAll { xs: List[Int] => {
       val actual = sumInParallelNot(xs)
       val expected = xs.sum
       actual should be (expected)
     }
    }
+  }
+
+  property("Sum in parallel") {
+    forAll { xss: List[Int] => {
+      val xs = 1 to 10  toList //bigger causes the current implementation of fork to block because it splits the problem into more pieces than I have threads.  I will fix that later in the chapter.
+      val actual = sumInParallel(xs)(executor)
+      val expected = xs.sum
+      actual.get should be (expected)
+    }
+   }
+  }
+
+  property("use lazyUnit") {
+    val actual = Par.lazyUnit(12)(executor).get
+    val ggg =Par.unit(12)(executor).get
+    actual should be (ggg)
   }
 }

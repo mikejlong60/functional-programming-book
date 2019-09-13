4a81a31f5983282c5b059d5a9a80cdd0b79bf306
Michael Long <michael.long@deciphernow.com>
2019-09-13 15:06:35 -0400

    Messes around with State traverse.  Not mastered yet.

--- src/test/scala/chapter11/StateTest.scala
+++ src/test/scala/chapter11/StateTest.scala
@@ -7,11 +7,10 @@ import org.scalatest.{Matchers, PropSpec}
 class StateTest extends PropSpec with PropertyChecks with Matchers {
 
   import Monad._
-  val mon = stateMonad[Int]
 
-  val monC6 = stateMonad[chapter6.RNG]
 
   property("Test State map function for Ints") {
+    val mon = stateMonad[Int]
     forAll { x: Int =>
       val s = State( (xx: Int) => (xx, xx))
       val actual = mon.map(s)((x:  Int) => x.toString).run(x)
@@ -20,14 +19,33 @@ class StateTest extends PropSpec with PropertyChecks with Matchers {
   }
 
   property("Use map") {
+    val mon = stateMonad[chapter6.RNG]
     forAll {x: Int =>
       whenever (x < 4000000) {
-      val rng = chapter6.SimpleRNG(x)
-      val (i, r) = rng.nextInt
-      val s = State(run = (s: chapter6.RNG) => (i, r))
-      val (i2,r2) = monC6.map(s)((x: Int) => x + 1200).run(rng)
-        i2 should be  (i + 1200)
-        println(r2)//r2 should be (true)
+        val rng = chapter6.SimpleRNG(x)
+        val (i, r) = rng.nextInt
+        val s = State(run = (s: chapter6.RNG) => (i, r))
+        val (i1,r1) = mon.map(s)((x: Int) => x + 1200).run(rng)
+        val (i2,r2) = mon.map(s)((x: Int) => x + 1200).run(rng)
+        val ii: (Int, chapter6.RNG) = mon.map(s)((x: Int) => x + 10).run(rng)
+        val ii2: (Int, chapter6.RNG) = mon.map(s)((x: Int) => x + 11).run(rng)
+        i1 should be  (i + 1200)
+        println(r1)//r2 should be (true)
+
+
+
+        val l = List(ii,ii2)//s, s)//(i1, r1), (i2, r2))
+        val kk = mon.traverse(l)(p  => {
+          println("p:"+p)
+          val pp = State(run = (s: chapter6.RNG) => (p._1, p._2))
+          println("pp"+pp)
+          pp
+        })
+        println("dude:"+kk)
+        mon.map(kk)(x => {
+          println("x:"+x)
+          x
+        }).run(rng)
       }
     }
   }

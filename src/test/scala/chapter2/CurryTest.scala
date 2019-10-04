bfe7001df5dd6674c99ce8f3ca17c100591af9ba
Michael Long <michael.long@deciphernow.com>
2019-10-04 10:17:25 -0400

    Gains a bit more understanding of currying and uncurrying.

--- src/test/scala/chapter2/CurryTest.scala
+++ src/test/scala/chapter2/CurryTest.scala
@@ -5,29 +5,29 @@ import org.scalatest.{Matchers, PropSpec}
 
 class CurryTest extends PropSpec with PropertyChecks with Matchers {
 
-  def curry[A, B, C](f: (A, B) => C): A => (B => C) = a => b => f(a, b)
+  def curry3[A, B, C, D](f: (A, B, C) => D): A => (B => (C => D)) = a => b => c => f(a, b, c)
 
   def partial1[A, B, C](a: A, f: (A, B) => C): B => C = (b: B) => f(a, b)
 
-  def uncurry[A, B, C](f: A => B => C): (A, B) => C = (a: A, b: B) => f(a)(b)
+  def uncurry3[A, B, C, D](f: A => B => C => D): (A, B, C) => D = (a: A, b: B, c: C) => f(a)(b)(c)
 
   property("Test curry and uncurry function with a Short and a Long") {
 
-    forAll { (n: Short, o: Long) =>
-      val cat: (Short, Long) => String = (x: Short, y: Long) => s"$x : $y"
-      val curried = curry(cat)
-      val uncurried = uncurry(curried)
-      val res = uncurried(n, o)
-      (curried(n)(o)) should be(uncurried(n, o))
+    forAll { (n: Short, o: Long, p: Long) =>
+      val cat: (Short, Long, Long) => String = (x: Short, y: Long, z: Long) => s"$x : $y : $z"
+      val curried: (Short) => (Long) => (Long) => String = curry3(cat)
+      val uncurried: (Short, Long, Long) => String = uncurry3(curried)
+      val res: String = uncurried(n, o, p)
+      (curried(n)(o)(p)) should be(uncurried(n, o, p))
     }
   }
 
   property("Test curry and uncurry function with a Short array and a Long array") {
-    forAll { (n: Array[Short], o: Array[Long]) =>
-      val cat: (Array[Short], Array[Long]) => String = (x: Array[Short], y: Array[Long]) => s"${x.mkString(",")} : ${y.mkString(",")}"
-      val curried = curry(cat)
-      val uncurried = uncurry(curried)
-      (curried(n)(o)) should be(uncurried(n, o))
+    forAll { (n: Array[Short], o: Array[Long], p: Array[Long]) =>
+      val cat: (Array[Short], Array[Long], Array[Long]) => String = (x: Array[Short], y: Array[Long], z: Array[Long]) => s"${x.mkString(",")} : ${y.mkString(",")} : ${z.mkString(",")}"
+      val curried: (Array[Short]) => (Array[Long]) => (Array[Long]) => String = curry3(cat)
+      val uncurried: (Array[Short], Array[Long], Array[Long]) => String = uncurry3(curried)
+      (curried(n)(o)(p)) should be(uncurried(n, o, p))
     }
   }
 }

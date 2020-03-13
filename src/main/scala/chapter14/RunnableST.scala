d973ec2d5b72b8c44f8ddf874d24c400124bf7c6
Michael Long <michael.long@deciphernow.com>
2020-03-13 13:58:51 -0400

    Comes back to chapter14 after vacation

--- src/main/scala/chapter14/RunnableST.scala
+++ src/main/scala/chapter14/RunnableST.scala
@@ -4,23 +4,3 @@ trait RunnableST[A] {
   def apply[S]: ST[S, A]
 }
 
-object RunnableST {
-  def main(args: Array[String]): Unit = {
-    val p = new RunnableST[(Int, Int)] {
-      def apply[S] = for {
-        r1 <- STRef(1)
-        r2 <- STRef(2)
-        x <- r1.read
-        y <- r2.read
-        _ <- r1.write(y + 1)
-        _ <- r2.write(x + 1)
-        a <- r1.read
-        b <- r2.read
-      } yield (a, b)
-    }
-
-    val r = ST.runST(p)
-    println(r)
-
-  }
-}
--- src/main/scala/chapter14/ST.scala
+++ src/main/scala/chapter14/ST.scala
@@ -18,7 +18,7 @@ sealed trait ST[S, A] { self =>
 }
 
 object ST {
-  def apply[S, A](a: => A): Object with ST[S, A] = {
+  def apply[S, A](a: => A): ST[S, A] = {
     lazy val memo = a
     new ST[S,A] {
       def run(s: S) = (memo, s)
@@ -45,4 +45,38 @@ object STRef {
   def apply[S, A](a: A): ST[S, STRef[S, A]] = ST(new STRef [S, A] {
     var cell = a
   })
+}
+
+sealed abstract class STArray[S, A](implicit manifest: Manifest[A]) {
+  protected def value: Array[A]
+
+  def size: ST[S, Int] = ST(value.size)
+
+  def write(i: Int, a: A):ST[S, Unit] = new ST[S, Unit]  {
+    def run(s: S) = {
+      value(i) = a
+      ((),  s)
+    }
+  }
+
+  def read(i: Int): ST[S, A] = ST(value(i))
+
+  def freeze: ST[S, List[A]] =ST(value.toList)
+
+  //This function makes the naive assumption that the list of keys is a list of ints(not necessarily in order) containing 0 and proceding in increments of 1.
+  def fill(xs: Map[Int, A]): ST[S, Unit] = xs.foldRight(ST[S,Unit](())) {
+    case ((k, v), st) => st flatMap (_ => write(k, v))
+  }
+}
+
+object STArray {
+  def apply[S, A: Manifest](sz: Int, v: A): ST[S, STArray[S, A]] =
+    ST(new STArray[S,  A] {
+      lazy val value = Array.fill(sz)(v)
+    })
+
+  def fromList[S, A:Manifest](xs: List[A]): ST[S, STArray[S, A]] = ST(new STArray[S, A] {
+    lazy val value = xs.toArray
+  })
+
 }

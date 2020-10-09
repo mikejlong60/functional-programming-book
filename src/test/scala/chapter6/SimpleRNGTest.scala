c363f375088f614e8a49eb4694db2be7e2d40401
Michael J Long <michael.long@greymatter.io>
2020-10-09 14:03:12 -0400

    Removed redundant class

--- src/test/scala/chapter6/SimpleRNGTest.scala
+++ src/test/scala/chapter6/SimpleRNGTest.scala
@@ -11,26 +11,26 @@ object SimpleRNGTest extends Properties("RNG tests") {
       val rng = SimpleRNG(x)
       val (n1, rng2) = rng.nextInt
       val (n2, rng3) = rng.nextInt
-      n1 ==n2
+      n1 == n2
     }
 
   property("Generate a non-negative random number") =
-    forAll {x: Int =>
+    forAll { x: Int =>
       val rng = SimpleRNG(x)
       val actual = nonNegativeInt(rng: RNG)
       actual._1 >= 0
     }
 
   property("Generate a non-negative double between 0 and 1") =
-    forAll {x: Int =>
+    forAll { x: Int =>
       val rng = SimpleRNG(x)
       val actual = double(rng: RNG)
       actual._2 != rng
-      actual._1.toInt ==  0
+      actual._1.toInt == 0
     }
 
   property("Generate pairs of random numbers") =
-    forAll {x: Int =>
+    forAll { x: Int =>
       val rng = SimpleRNG(x)
       val actual = intDouble(rng)
       actual._2 != rng
@@ -39,7 +39,7 @@ object SimpleRNGTest extends Properties("RNG tests") {
     }
 
   property("Generate pairs of random numbers in opposite order") =
-    forAll {x: Int =>
+    forAll { x: Int =>
       val rng = SimpleRNG(x)
       val actual = doubleInt(rng)
       actual._2 != rng
@@ -48,7 +48,7 @@ object SimpleRNGTest extends Properties("RNG tests") {
     }
 
   property("Generate triple of random numbers") =
-    forAll {x: Int =>
+    forAll { x: Int =>
       val rng = SimpleRNG(x)
       val actual = double3(rng)
       actual._2 != rng
@@ -58,26 +58,26 @@ object SimpleRNGTest extends Properties("RNG tests") {
     }
 
   property("Generate list of random Ints") =
-    forAll {x: Short =>
+    forAll { x: Short =>
       (x > 0 && x < 4000) ==> {
         val rng = SimpleRNG(x)
-        val actual = ints(x)(rng)//Because this is not tail recursive it blows up.  The book solution shows a tail-recursive version with an inner function.
+        val actual = ints(x)(rng) //Because this is not tail recursive it blows up.  The book solution shows a tail-recursive version with an inner function.
         actual._1.size == x
       }
     }
 
   property("Generate a non-negative double between 0 and 1 with map") =
-    forAll {x: Int =>
+    forAll { x: Int =>
       val rng = SimpleRNG(x)
       val actual = doubleMap(rng)
       actual._2 != rng
       actual._1.toInt == 0
     }
- 
+
   property("Generate an Int and  Double pair using map2") =
-    forAll {x: Int =>
+    forAll { x: Int =>
       val rng = SimpleRNG(x)
-      val fff = map2(nonNegativeInt, doubleMap)((_,_))
+      val fff = map2(nonNegativeInt, doubleMap)((_, _))
       val actual = fff(rng)
       actual._2 != rng
       actual._1._1 >= 0
@@ -85,7 +85,7 @@ object SimpleRNGTest extends Properties("RNG tests") {
     }
 
   property("Generate a Double  Double pair using map2") =
-    forAll {x: Int =>
+    forAll { x: Int =>
       val rng = SimpleRNG(x)
       val fff = map2(doubleMap, doubleMap)((_, _))
       val actual = fff(rng)
@@ -95,13 +95,25 @@ object SimpleRNGTest extends Properties("RNG tests") {
     }
 
   property("Generate a sequence of random doubles") =
-    forAll{(xs: List[Int], x: Int) =>
+    forAll { (xs: List[Int], x: Int) =>
       val rng = SimpleRNG(x)
       val xxs = xs.map(x => doubleMap)
       val fff = sequence(xxs)
       val actual = fff(rng)
       actual._1.size == (xs.size)
     }
+
+
+  property("TestFlatMap") = {
+    val rng = SimpleRNG {
+      123
+    }
+    val r = unit(12)
+    val res = flatMap(r)(i => unit(i + 1))
+    val actual = res(rng)
+    val actual2 = res(actual._2)
+    actual == actual2
+  }
 
   def nonNegativeLessThan(n: Int): Rand[Int] = { rng =>
     val (i, rng2) = nonNegativeInt(rng)
@@ -111,35 +123,35 @@ object SimpleRNGTest extends Properties("RNG tests") {
   }
 
   def nonNegativeLessThanFm(n: Int): Rand[Int] = {
-     flatMap(nonNegativeInt)(i => {
-       val mod = i % n
-       if (i  +  (n - 1) - mod >= 0) unit(mod) //rng =>  (mod, rng)
-       else  nonNegativeLessThanFm(n)
+    flatMap(nonNegativeInt)(i => {
+      val mod = i % n
+      if (i + (n - 1) - mod >= 0) unit(mod) //rng =>  (mod, rng)
+      else nonNegativeLessThanFm(n)
     })
   }
 
   property("Use flatMap for nonNegativeLessThan") =
-    forAll{x: Int =>
+    forAll { x: Int =>
       (x > 0) ==> {
-      val rng = SimpleRNG(x)
-      val result = nonNegativeLessThanFm(x)
-      val actual = result(rng)
-      actual._1 >= 0
-      actual._1  < x
+        val rng = SimpleRNG(x)
+        val result = nonNegativeLessThanFm(x)
+        val actual = result(rng)
+        actual._1 >= 0
+        actual._1 < x
       }
     }
 
   property("Use flatMap") =
-    forAll{(xs: List[Int], x: Int) =>
-     val rng = SimpleRNG(x)
-     val xxs = xs.map(x => {
-       val r = unit(x)
-       val res = flatMap(r)(d => unit(x))
-       res
-     })
-     val fff = sequence(xxs)
-     val actual = fff(rng)
-     actual._1.size == (xs.size)
+    forAll { (xs: List[Int], x: Int) =>
+      val rng = SimpleRNG(x)
+      val xxs = xs.map(x => {
+        val r = unit(x)
+        val res = flatMap(r)(d => unit(x))
+        res
+      })
+      val fff = sequence(xxs)
+      val actual = fff(rng)
+      actual._1.size == (xs.size)
     }
 
   property("Reveal bug in roll die. This should fail") = {
